eeb12f7882f7cbeec69a540349d594eaf41fe54b
Yangguang Li <liyangguangcn@gmail.com>
2020-05-01 22:04:18 -0700

    UX improvements on the google map embed.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,17 +68,20 @@ h2 {
 }
 
 .iframe-container {
-  padding-bottom: 66%;
+  padding-bottom: 50%;
   position: relative;
+  pointer-events: none;
+  overflow: hidden;
 
   iframe {
+    --map-header-height: 50px;
     position: absolute;
-    top: 0;
+    top: calc(-1 * var(--map-header-height));
     bottom: 0;
     left: 0;
     right: 0;
     width: 100%;
-    height: 100%;
+    height: calc(100% + var(--map-header-height));
   }
 }
 
